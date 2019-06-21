98e4e71f81b5c392db07291bb55f13aacdd004c4
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-20 20:58:37 -0500

    Updates

---
+++ precache-manifest.a571dc787c75e384f9e55189e736ca2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75ab43381264c7a67270f376aa4d0c7",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e547992f501e0e10b718",
+    "url": "/react-tic-tac-toe/static/css/main.278145d6.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "e547992f501e0e10b718",
+    "url": "/react-tic-tac-toe/static/js/main.06e5d0ac.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  }
+]);
